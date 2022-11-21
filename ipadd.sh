61cd208f066d2a6b31901fe917b94b4f1ee22f67
Ramesh Tamang <11027110+rmstmg01@users.noreply.github.com>
2022-11-21 12:12:15 +0545

    Update ipadd.sh

--- ipadd.sh
+++ ipadd.sh
@@ -3,6 +3,8 @@
 # This script takes list of IPs from file ip.txt file and creates ip configuration file for each IP.
 # This script becomes handy when you need to add multiple IPs on the same network interface of RHEL based servers like CentOS/Fedora.
 #Please update ip.txt file, netmask and gateway variable as your need.
+# Run script ash bash ipadd.sh
+
 count=0
 netmask=255.255.255.0
 gateway=192.168.0.1
